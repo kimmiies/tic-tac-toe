0f5f41148bcff64522100068e6e7b8d8f03888ee
Kimberly Oleiro <koleiro@gmail.com>
2016-04-26 17:47:03 -0400

    Finalized clearBoard function, added AttachedHandler function, called this function at the top of the doc

--- ttt.js
+++ ttt.js
@@ -1,6 +1,7 @@
 $(document).on('ready', function() {
   var winningCombos = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7] ];
   var turn = 0;
+  attachHandler();
 
   function checkWinner() {
 
@@ -20,42 +21,36 @@ $(document).on('ready', function() {
             clearBoard();
             return true
 
-      
           } else if (m4 && m5 && m6){
             alert("O wins!");
             clearBoard();
             return true
-
           }
-
+      }
     return false
   };
 
 // need to return true or false or else it will return undefined.
 
-function checkTie() {
-  if (turn === 9 && (checkWinner() === false)){
-    alert("tie!");
-    clearBoard()
-  }
-};
+  function checkTie() {
+    if (turn === 9 && (checkWinner() === false)){
+      alert("tie!");
+      clearBoard()
+    }
+  };
 
 
-function clearBoard() {
-  $('td').removeClass('x').removeClass('o').html("")
-  turn = 0;
-};
+  function clearBoard() {
+    $('td').removeClass('x').removeClass('o').html("")
+    turn = 0;
+    attachHandler();
+  };
 
 
+  function attachHandler() {
+              // ensure click handler has been turned off for everything that may have been click before
+    $('td').off('click');
 
-
-// clear board function
-
- // get rid of classes and html text, reset turn and restart attachHandler method
-
-//   function attachHandler() {
-// // ensure click handler has been turned off for everything that may have been click before
-//     $(this).off('click');
       $('td').on('click', function (){
 
         if (turn % 2 === 0) {
@@ -63,18 +58,16 @@ function clearBoard() {
         }else {
           $(this).html('o').addClass('o');
         }
-
-// turn off the click handler for everything that has been clicked
+                  // turn off the click handler for everything that has been clicked
         turn++;
         $(this).off('click');
 
         checkWinner();
         checkTie();
 
-
-
       });
-  // }
+  };
+
 
 });
 
